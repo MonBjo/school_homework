a447c39ba35ef3246946db80ef8eb9a09c75ab84
Monica Björk <m.bjork95@gmail.com>
2022-01-19 20:55:51 +0100

    user functions

--- 06.Jacascript Basics/Objects/script.js
+++ 06.Jacascript Basics/Objects/script.js
@@ -33,3 +33,71 @@ function findBook(title) {
 }
 
 console.log(findBook("Båtäventyr på västkusten"));
+
+
+// letterFrequency("kalle") // => {"k": 1. "a": 1, "l": 2, "e": 1}
+
+console.log(letterFrequency("kalle"));
+
+function letterFrequency(word) {
+    word.toLowerCase();
+    let letters = [];
+
+    while (word != "") {
+    }
+    
+    return letters;
+}
+
+
+
+console.log(filterByCountry("FR"));
+
+function filterByCountry(country) {
+    let usersFilteredByCountry = [];
+    for(let i = 0; i < users.length; i++) {
+        if(users[i].nat == country){
+            usersFilteredByCountry.push(users[i]);
+        }
+    }
+    return usersFilteredByCountry;
+}
+
+
+console.log(filterByGender("Female"));
+
+// mr monsieur
+// miss ms mrs mademoiselle madame
+
+function filterByGender(gender) {
+    let usersFilteredByGender = [];
+
+    if(gender == "Male") {
+        gender = ["mr", "monsieur"];
+    } else if(gender == "Female"){
+        gender = ["miss", "ms", "mrs", "mademoiselle", "madame"];
+    } else {
+        console.log(`please choose "Male" or "Female".`);
+    }
+
+    for(let i = 0; i < users.length; i++) {
+        for(let j = 0; j < gender.length; j++) {
+            if(users[i].title == gender[i]){
+                usersFilteredByGender.push(users[i]);
+                break;
+            }
+        }
+    }
+    return usersFilteredByGender;
+}
+
+
+console.log(filterEmailOnly());
+
+function filterEmailOnly() {
+    let usersFilterEmail = [];
+    for(let i = 0; i < users.length; i++) {
+        usersFilterEmail.push(users[i].email);
+    }
+    return usersFilterEmail;
+}

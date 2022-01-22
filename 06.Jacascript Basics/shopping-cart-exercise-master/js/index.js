4847e281ac8798d85bb8a56d42f34d2e3b0f78a0
Monica Björk <m.bjork95@gmail.com>
2022-01-22 14:21:56 +0100

    So confused

--- 06.Jacascript Basics/shopping-cart-exercise-master/js/index.js
+++ 06.Jacascript Basics/shopping-cart-exercise-master/js/index.js
@@ -1,31 +1,42 @@
-//Variables to use and it's a mix of global and local variables
-let shoppingCart = [];
-let products = document.getElementsByTagName('button');
 let cartProducts = '';
-
-//This code gets whatever is inside data-product in the HTML
-let product = event.target.parentNode.getAttribute('data-product');
-cartProducts += '<li><span class="product-title">Titel: </span>' + shoppingCart[i] + '</li>';
-shoppingCart.push(product);
-
-//
-document.getElementById('productsInCart').innerHTML = shoppingCart.length;
-document.getElementById('cart').classList.toggle('hide');
-document.getElementById('products').innerHTML = cartProducts;
-products[i].addEventListener('click', function(event) {});
-document.getElementById('open-cart').addEventListener('click', function() {});
-
-//Function declarations, add code inside {}
-function updateCart() {}
-function listProductsInCart() {}
+let products = document.getElementsByTagName('button');
 
 
-//For-loops of two arrays
-for(let i = 0; i < shoppingCart.length; i++) {}
-for(let i = 0; i < products.length; i++) {}
 
 
-//Function calls and there should be two listProductsInCart()
-updateCart();
-listProductsInCart();
-listProductsInCart();
+
+document.getElementById('open-cart').addEventListener('click', function() {
+    listProductsInCart();
+});
+
+
+
+
+function listProductsInCart() {
+    document.getElementById('cart').classList.toggle('hide');
+    
+    document.getElementById('productsInCart').innerHTML = shoppingCart.length;
+    
+    for(let i = 0; i < shoppingCart.length; i++) {
+        cartProducts += '<li><span class="product-title">Titel: </span>' + shoppingCart[i] + '</li>';
+    }
+}
+
+
+let shoppingCart = [];
+function updateCart() {
+    document.getElementById('products').innerHTML = cartProducts;
+    listProductsInCart();
+}
+
+for(let i = 0; i < products.length; i++) {
+    products[i].addEventListener('click', function(event) {
+        updateCart();
+        let product = event.target.parentNode.getAttribute('data-product');
+        shoppingCart.push(product);
+    });
+}
+
+
+/*
+*/

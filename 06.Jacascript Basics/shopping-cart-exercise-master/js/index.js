f8bb37f23c7cc4c0d40008cc7b4db8cd04c31d16
Monica Björk <m.bjork95@gmail.com>
2022-01-24 09:53:41 +0100

    Shopping Cart done with class

--- 06.Jacascript Basics/shopping-cart-exercise-master/js/index.js
+++ 06.Jacascript Basics/shopping-cart-exercise-master/js/index.js
@@ -1,42 +1,33 @@
-let cartProducts = '';
 let products = document.getElementsByTagName('button');
+let shoppingCart = [];
 
 
+listProductsInCart();
+document.getElementById('cart').classList.toggle('hide');
 
-
-
-document.getElementById('open-cart').addEventListener('click', function() {
-    listProductsInCart();
-});
-
-
-
+function updateCart() {
+    document.getElementById('productsInCart').innerHTML = shoppingCart.length;
+}
 
 function listProductsInCart() {
-    document.getElementById('cart').classList.toggle('hide');
-    
-    document.getElementById('productsInCart').innerHTML = shoppingCart.length;
+    let cartProducts = '';    
     
     for(let i = 0; i < shoppingCart.length; i++) {
         cartProducts += '<li><span class="product-title">Titel: </span>' + shoppingCart[i] + '</li>';
     }
-}
-
-
-let shoppingCart = [];
-function updateCart() {
+    
     document.getElementById('products').innerHTML = cartProducts;
-    listProductsInCart();
 }
 
 for(let i = 0; i < products.length; i++) {
     products[i].addEventListener('click', function(event) {
-        updateCart();
         let product = event.target.parentNode.getAttribute('data-product');
         shoppingCart.push(product);
+        
+        updateCart();
+        listProductsInCart();
     });
 }
 
-
-/*
-*/
+document.getElementById('open-cart').addEventListener('click', function() {
+});

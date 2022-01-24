c23dcc732de2fbd5d98ef10053a8c238e5146416
Monica Björk <m.bjork95@gmail.com>
2022-01-24 15:01:38 +0100

    working with the DOM tree

--- 11.DOMträdet/sinus-minishop/script.js
+++ 11.DOMträdet/sinus-minishop/script.js
@@ -1,1 +1,22 @@
-// Code here!
+console.log(document.getElementsByClassName("art-1")[0].getElementsByTagName("h3")[0].innerText = "Potato");
+
+console.log(document.getElementsByTagName("nav")[0].getElementsByTagName("a")[0].innerText = "Start");
+console.log(document.getElementsByTagName("footer")[0].getElementsByTagName("article")[0].getElementsByTagName("a")[0].innerText = "Start");
+
+console.log(document.getElementsByTagName("nav")[0].getElementsByTagName("a")[2].innerText = "Mail us");
+console.log(document.getElementsByTagName("footer")[0].getElementsByTagName("article")[0].getElementsByTagName("a")[2].innerText = "Mail us");
+
+console.log(document.getElementsByClassName("art-2")[0].getElementsByTagName("h2")[0].innerText = "Sinus Jacket");
+console.log(document.getElementsByClassName("art-2")[0].getElementsByTagName("h3")[0].innerText = "Lava");
+
+console.log(document.getElementsByClassName("art-1")[0].getElementsByTagName("button")[0].innerText = "Get me");
+console.log(document.getElementsByClassName("art-1")[0].getElementsByTagName("button")[0].style.backgroundColor = "gray");
+
+console.log(document.getElementsByClassName("art-3")[0].style.backgroundColor = "yellow");
+
+console.log(document.getElementsByTagName("footer")[0].getElementsByTagName("article")[1].getElementsByTagName("p")[0].innerHTML = "Sinus Skateboards<br>Storgatan 58G<br>430 12, Hoppstropp");
+
+let paragraphs = document.getElementsByTagName("p");
+for(let paragraph of paragraphs){
+    console.log(paragraph.style.backgroundColor = "lightblue");
+}

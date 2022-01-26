99eab9b0d4ae12cd990e410df05386dc10bd97e6
Monica Björk <m.bjork95@gmail.com>
2022-01-26 21:28:33 +0100

    Halfway there. Flipping works and you can win

--- 11.Blandade uppgifter/memory-game-exercise/js/index.js
+++ 11.Blandade uppgifter/memory-game-exercise/js/index.js
@@ -2,9 +2,9 @@ let cardsElem = document.querySelectorAll('.memory-card');
 let winElem = document.querySelectorAll('.overlay');
 let cardsToCompare = [];
 let pairsFound = 0;
-//console.log(cardsElem);
+console.log(cardsElem);
 
-for(let card of cardsElem){
+for(let card of cardsElem) {
     //console.log("card: ", card);
     //console.log(card.attributes[1].value);
     
@@ -13,27 +13,62 @@ for(let card of cardsElem){
         //console.log(`click card: `, card);
         //console.log(`click event: `, event);
         card.classList.add('flip');
-
-        //let value = event.target.parentNode.getAttribute('data-card');
-        let value = card.attributes[1].value;
-        console.log(`value: `, value);
-        cardsToCompare.push(value);
-
-        if(cardsToCompare.length == 2) {
-            checkMatch();
-        }
+        
+        compareCards(card);
+        
     });
 }
 
-function checkMatch(card){
-    console.log('cards to compare: ', cardsToCompare);
-    if(cardsToCompare[0] == cardsToCompare[1]){
-        console.log('lika');
-        pairsFound++;
-    } else {
-        console.log('olika');
+function compareCards(card){
+
+    setTimeout(() => {
+        let value = card.attributes[1].value;
+        console.log(`value: `, value);
+        cardsToCompare.push({value: value, card: card});
+    
+        if(cardsToCompare.length == 2) {
+            if(!checkMatch()) {
+                
+            }
+            cardsToCompare = [];
+            console.log(`Pairs so far: ${pairsFound}`);
+            console.log('----------');
+        }
+    }, 1500);
+
+
+    function checkMatch(){
+        console.log('cards to compare: ', cardsToCompare);
+        if(cardsToCompare[0].value == cardsToCompare[1].value){
+            pairsFound++;
+            if(pairsFound == 8){
+                overlayWin(); 
+            }
+            return true;
+        } else {
+            flipBack();
+            return false;
+        }
     }
-    cardsToCompare = [];
-    console.log(`Pairs so far: ${pairsFound}`);
-    console.log('----------');
 }
+
+
+function flipBack(){
+    console.log(cardsToCompare);
+    for(let card of cardsElem) {
+        for(let value of cardsToCompare){
+            if(card.attributes[1].value == value.value){
+                card.classList.remove('flip');
+            }
+            //console.log(card.attributes[1].value + " | " + value.value);
+        }
+    }
+}
+
+
+function overlayWin(){
+    console.log(winElem[0]);
+    winElem[0].classList.add('show');
+}
+
+// TODO: add so the exit button works

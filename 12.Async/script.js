0dda289c8e18ab685063615fae08bb7725ec42a5
Monica Björk <m.bjork95@gmail.com>
2022-02-09 11:42:10 +0100

    chained promises

--- 12.Async/script.js
+++ 12.Async/script.js
@@ -20,3 +20,53 @@ function letsDance(danceStyle) {
         console.log("reject: ", value);
     });
 }
+
+
+
+webProject();
+
+function webProject(){
+    let doWebProject = new Promise(function(resolve, reject) {
+        setTimeout(() => resolve("Researcha målgruppen"), 4000);
+    }).then(function(progress) {
+        console.log(progress);
+        
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve("Skissa upp en design på papper"), 2000);
+        });
+    }).then(function(progress) {
+        console.log(progress);
+        
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve("Gör skissen till pixel perfect mockup I Figma / XD"), 3000);
+        });
+    }).then(function(progress) {
+        console.log(progress);
+        
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve("Koda"), 3000);
+        });
+    }).then(function(progress) {
+        console.log(progress);
+        
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve("Stackoverflow:a fel"), 1000);
+        });
+    }).then(function(progress) {
+        console.log(progress);
+        
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve("Testa produkten"), 2000);
+        });
+    }).then(function(progress) {
+        console.log(progress);
+        
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve("Fira lyckat projekt"), 1000);
+        });
+    }).then(function(progress) {
+        console.log(progress);
+    });
+
+    console.log("doWebProject: ", doWebProject);
+}

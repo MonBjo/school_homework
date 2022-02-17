551cc165c94c0a28c6004350d32340a4997671ae
Monica Björk <m.bjork95@gmail.com>
2022-02-17 15:20:46 +0100

    nope

--- 13.APIs/flickr/script.js
+++ 13.APIs/flickr/script.js
@@ -1,18 +1,17 @@
 const searchFieldElem = document.querySelector('#searchField');
-const photosElem = document.querySelector('#photos');
+const thumbnailContaierElem = document.querySelector('#thumbnailContaier');
+const lightboxElem = document.querySelector('#lightbox');
 
 const API_KEY = `45ee5e5758f149d9560aff38950ac9d9`;
 const SECRET = `34b61fb57ef5adfd`;
 const BASE_URL = `https://api.flickr.com/services/rest`;
 const METHOD = `?method=flickr.photos.search`;
+const EXTRAS = `description,owner_name,icon_server`;
 const SEARCH_AMOUNT = 25;
 
-for(let i = 0; i < SEARCH_AMOUNT; i++) {
-    photosElem.innerHTML += `<div class="placeholderImg"> </div>`;
-}
+
 searchFieldElem.addEventListener('keypress', function(event) {
-    console.log(event);
-    
+    //console.log(event);
     if(event.keyCode == 13){ 
         let searchText = event.target.value;
 
@@ -24,33 +23,34 @@ searchFieldElem.addEventListener('keypress', function(event) {
 
 
 async function getPhotos(searchText) {
-    const response = await fetch(`${BASE_URL}${METHOD}&api_key=${API_KEY}&text=${searchText}&per_page=${SEARCH_AMOUNT}&content_type=1&sort=relevance&format=json&nojsoncallback=1`);
+    const response = await fetch(`${BASE_URL}${METHOD}&api_key=${API_KEY}&text=${searchText}&per_page=${SEARCH_AMOUNT}&content_type=1&sort=relevance&extras=${EXTRAS}&format=json&nojsoncallback=1`);
     const data = await response.json();
-    console.log("data", data);
+    // console.log("data", data);
 
     const photos = data.photos.photo;
     console.log(`photos of "${searchText}"`, photos);
 
     displayPhotos(photos);
-    lightbox();
 }
 
-function lightbox(){
-    const photos = photosElem.querySelectorAll('.searchedPhoto');
+function displayPhotos(photos) {
+    thumbnailContaierElem.innerHTML = ` `;
     for(let photo of photos) {
-        photo.addEventListener('click', function(event) {
-            console.log("clickety clakety");
+        const photoURLThumbnail = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`;
+        thumbnailContaierElem.innerHTML += `<img class="searchedPhotoThumbnail" src="${photoURLThumbnail}">`;
+        
+        //const photoURLLarge = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`;
+        //lightboxElem.innerHTML += `<img class="lightbox-photo" src="${photoURLLarge}">`;
+    }
+    addThumbnailEvent();
+}
+
+function addThumbnailEvent() {
+    const thumbnailsElems = thumbnailContaierElem.querySelectorAll('.searchedPhotoThumbnail');
+    for(let thumbnail of thumbnailsElems) {
+        thumbnail.addEventListener('click', function() {
+            console.log("this", this.src);
         });
     }
 }
 
-
-function displayPhotos(searchedPhotos) {
-    photosElem.innerHTML = ` `;
-    for(let photo of searchedPhotos) {
-        const photoURLThumbnail = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`;
-        photosElem.innerHTML += `<img class="searchedPhotoThumbnail" src="${photoURLThumbnail}">`;
-        const photoURLLarge = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`;
-    }
-    photosElem.querySelectorAll('.searchedPhoto');
-}

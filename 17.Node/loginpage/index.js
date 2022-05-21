532e116ce6e974b5ea3b3c3dbad7289078182a85
Monica Björk <m.bjork95@gmail.com>
2022-05-21 16:20:07 +0200

    Compare users to login

--- 17.Node/loginpage/index.js
+++ 17.Node/loginpage/index.js
@@ -4,17 +4,45 @@ const port = 8000;
 
 app.use(express.json());
 
-const user = [{
-  email: "john.doe@example.com",
-  password: "qwerty123"
-}];
+const users = [
+  {
+    username: "lean",
+    email: "lena.andersson@example.com",
+    password: "qwerty123"
+  }, 
+  {
+    username: "kibe",
+    email: "kim.bergquist@example.com",
+    password: "qwerty123"
+  }, 
+  {
+    username: "jodo",
+    email: "john.doe@example.com",
+    password: "qwerty123"
+  }    
+];
 
 app.get('/', (req, res) => {
   res.send('Snel helst');
 });
 
 app.get('/api/login', (req, res) => {
-  res.send('login?');
+  let login = {
+    username: "kibe",
+    email: "kim.bergquist@example.com",
+    password: "qwerty123"
+  };
+  let filterUsers = users.filter((user) => {
+    console.log(user.username == login.username);
+    console.log(user.email == login.email);
+    return user;
+  });
+  
+  res.send('trying to log in');
+});
+
+app.get('/api/signup', (req, res) => {
+  res.send('sign up?');
 });
 
 app.listen(port, () => {

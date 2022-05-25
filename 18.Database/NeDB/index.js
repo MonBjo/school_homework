ca8c77583193987ee46c69462b1860dba0f026ea
Monica Björk <m.bjork95@gmail.com>
2022-05-25 16:08:11 +0200

    first database

--- 18.Database/NeDB/index.js
+++ 18.Database/NeDB/index.js
@@ -1,5 +1,16 @@
 const nedb = require('nedb-promise');
 const database = new nedb({ filename: 'database.db', autoload: true });
 
-//database.insert({ firstname: 'Kristina', lastname: 'Lund', age: 43 });
+//database.insert({ firstname: 'Helena', lastname: 'Lund', age: 33 });
 
+async function getPersons() {
+  //const persons = await database.find({ lastname: 'Svensson' });
+  //const persons = await database.find({ firstname: 'Kristina' });
+  //const persons = await database.find({ age: { $gt: 30 } });
+  //console.log(persons);
+}
+
+getPersons();
+
+//database.update({ firstname: 'Helena' }, { $set: { firstname: 'Hanna', age: 30 } }); 
+database.remove({ firstname: 'Hanna' }); 

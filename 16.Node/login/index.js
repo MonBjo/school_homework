7895dd45305e34c44a8098b6869736681e129df1
Monica Björk <m.bjork95@gmail.com>
2022-06-01 15:39:34 +0200

    meh

--- 16.Node/login/index.js
+++ 16.Node/login/index.js
@@ -3,7 +3,28 @@
 
 // Skapa två endpoints i din Node.JS - fil som heter /api/login och /api/signup.
 // /api/login - tar användarnamn och lösenord som har skickats in och kollar om det finns samt är samma som i databasen. Endpoint:en ska returnera ett objekt med egenskapen success som antingen är true eller false till klienten.
-
 // /api/signup - tar emot användarnamn, lösenord, och e-post och kontrollerar så att användarnamn och e-post inte redan finns. Endpoint:en ska returnera ett objekt med egenskaparna: success, usernameExists och emailExists. Alla egenskapar är antingen true eller false.
-
 // Allt kan sparas i variabel på din server.
+
+const { request } = require('express');
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.send(`ohai`);
+});
+
+app.get('/api/login', (request, response) => {
+
+});
+
+app.get('/api/signup', (request, response) => {
+  
+});
+
+app.listen(PORT, () => {
+  console.log(`Servern är startad på port: ${PORT}`);
+});
